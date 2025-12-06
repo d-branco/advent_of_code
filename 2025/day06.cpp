9af29fa57c7ee33054a9f3ada6713c6354d0ebde
gris <185072671+d-branco@users.noreply.github.com>
2025-12-06 10:57:36 +0000

    2025day05P1 init

--- 2025/day06.cpp
+++ 2025/day06.cpp
@@ -1,11 +1,11 @@
 /* ************************************************************************** */
 /*                                          ::::::::    ::::::::   :::::::::  */
-/*   day03.cpp                            :+:    :+:  :+:    :+:  :+:         */
+/*   day06.cpp                            :+:    :+:  :+:    :+:  :+:         */
 /*                                             +:+         :+:   :+:          */
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
-/*   Created: 2025/12/03 06:54:07      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/12/06 10:28:06     #########  #########  ###      ###      */
+/*   Created: 2025/12/06 10:28:06      #+#         #+#      +#+        #+#    */
+/*   Updated: 2025/12/06 10:57:01     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ int	 main(int argc, char **argv)
 	{
 		return (EXIT_FAILURE);
 	}
+	dprint("");
 
 	part_one(file);
 	file.clear();
@@ -51,34 +52,7 @@ void part_two(std::ifstream &file)
 
 void part_one(std::ifstream &file)
 {
-	unsigned long count = 0;
-	std::string	  input_line;
-	while (std::getline(file, input_line) != 0)
-	{
-		dprint("Line: " << input_line);
-		char nbr1 = 0;
-		char nbr2 = 0;
-		for (unsigned int i = 0; i < input_line.size(); i++)
-		{
-			if ((input_line[i] > nbr1) && (i != (input_line.size() - 1)))
-			{
-				nbr1 = input_line[i];
-				nbr2 = 0;
-				continue;
-			}
-			if (input_line[i] > nbr2)
-			{
-				nbr2 = input_line[i];
-			}
-		}
-		dprint("nbr1 " << nbr1);
-		dprint("nbr2 " << nbr2);
-		unsigned int nbr = (((nbr1 - '0') * 10) + (nbr2 - '0'));
-		dprint("sum: " << nbr);
-		count += nbr;
-	}
-
-	std::cout << "Part one: Sum: " << count << "\n";
+	(void) file;
 }
 
 int input_validation(int argc, char **argv, std::ifstream &file)
@@ -97,7 +71,6 @@ int input_validation(int argc, char **argv, std::ifstream &file)
 	dprint("Input file: " << INPUT_FILE);
 
 	dprint("Checking if file exists and is readable");
-	// std::ifstream file = file(INPUT_FILE);
 	if (!file.good())
 	{
 		std::cout << "Error: Could nor access file " << INPUT_FILE << "\n";
