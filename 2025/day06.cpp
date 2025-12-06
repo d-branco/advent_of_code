47c19b2852df9cc151c7b319884d738895cbda50
gris <185072671+d-branco@users.noreply.github.com>
2025-12-06 12:05:17 +0000

    The return of long long

--- 2025/day06.cpp
+++ 2025/day06.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/12/06 10:28:06      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/12/06 10:57:01     #########  #########  ###      ###      */
+/*   Updated: 2025/12/06 12:04:49     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -52,7 +52,68 @@ void part_two(std::ifstream &file)
 
 void part_one(std::ifstream &file)
 {
-	(void) file;
+	std::string lin1;
+	std::string lin2;
+	std::string lin3;
+	std::string lin4;
+	std::string oper;
+
+	std::getline(file, lin1);
+	std::getline(file, lin2);
+	std::getline(file, lin3);
+	std::getline(file, lin4);
+	std::getline(file, oper);
+
+	size_t			   pos = 0;
+	size_t			   i;
+	unsigned long long eins;
+	unsigned long long zwei;
+	unsigned long long drei;
+	unsigned long long vier;
+	char			   op;
+	(void) op;
+	unsigned long long temp;
+	unsigned long long sum = 0;
+	while (pos < oper.length())
+	{
+		for (i = 0; oper[pos + i] != '\0'; i++)
+		{
+			if ((lin1[pos + i] == ' ') && (lin2[pos + i] == ' ')
+				&& (lin3[pos + i] == ' ') && (lin4[pos + i] == ' ')
+				&& (oper[pos + i] == ' '))
+			{
+				break;
+			}
+		}
+
+		eins = std::atoi(lin1.substr(pos, i).c_str());
+		zwei = std::atoi(lin2.substr(pos, i).c_str());
+		drei = std::atoi(lin3.substr(pos, i).c_str());
+		vier = std::atoi(lin4.substr(pos, i).c_str());
+		if (oper[pos] == '+')
+		{
+			op	 = '+';
+			temp = eins + zwei + drei + vier;
+		}
+		else if (oper[pos] == '*')
+		{
+			op	 = '*';
+			temp = eins * zwei * drei * vier;
+		}
+		else
+		{
+			std::cerr << "Error: could not find operator!\n";
+			return;
+		}
+		sum += temp;
+		dprint("substr size: " << i << " -> \'" << eins << "\' " << op << " \'"
+							   << zwei << "\' " << op << " \'" << drei << "\' "
+							   << op << " \'" << vier << "\' = " << temp
+							   << "  sum: " << sum);
+		pos = pos + i + 1;
+	}
+
+	std::cout << "Part one: Grand total: " << sum << "\n";
 }
 
 int input_validation(int argc, char **argv, std::ifstream &file)
